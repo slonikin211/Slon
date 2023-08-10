a51ea4cedf5fbdfdd3a22dd91d34b752d701569a
slonikin211 <85413294+slonikin211@users.noreply.github.com>
2023-08-10 18:30:28 +0400

    Add interfaces for CQRS pattern (#4)

---
+++ Slon.SharedAssets/Application/Messaging/IQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Slon.SharedAssets.Common;
+
+namespace Slon.SharedAssets.Application.Messaging;
+
+/// <summary>
+/// Represents the base interface for a query with a response.
+/// </summary>
+/// <typeparam name="TResponse">The type of the response expected from the query.</typeparam>
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+{
+}
