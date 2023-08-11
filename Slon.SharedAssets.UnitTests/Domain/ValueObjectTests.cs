b6eb16a1d11a10df0ac02b2e0734b215a42ca752
slonikin211 <georgepark.dev@gmail.com>
2023-08-11 14:52:23 +0400

    Add value objects

---
+++ Slon.SharedAssets.UnitTests/Domain/ValueObjectTests.cs
@@ -0,0 +1,87 @@
+﻿using Slon.SharedAssets.Domain;
+
+namespace Slon.SharedAssets.UnitTests.Domain;
+
+public class ValueObjectTests
+{
+    private sealed class TestValueObject : ValueObject
+    {
+        public string Value1 { get; }
+        public int Value2 { get; }
+
+        public TestValueObject(string value1, int value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value1;
+            yield return Value2;
+        }
+    }
+
+    [Fact]
+    public void Equals_SameObject_ReturnsTrue()
+    {
+        var obj = new TestValueObject("abc", 123);
+
+        Assert.True(obj.Equals(obj));
+    }
+
+    [Fact]
+    public void Equals_NullObject_ReturnsFalse()
+    {
+        var obj = new TestValueObject("abc", 123);
+
+#pragma warning disable CA1508 // Avoid dead conditional code
+        Assert.False(obj.Equals(null));
+#pragma warning restore CA1508 // Avoid dead conditional code
+    }
+
+    [Fact]
+    public void Equals_DifferentType_ReturnsFalse()
+    {
+        var obj = new TestValueObject("abc", 123);
+        var differentType = new object();
+
+        Assert.False(obj.Equals(differentType));
+    }
+
+    [Fact]
+    public void Equals_SameValues_ReturnsTrue()
+    {
+        var obj1 = new TestValueObject("abc", 123);
+        var obj2 = new TestValueObject("abc", 123);
+
+        Assert.True(obj1.Equals(obj2));
+    }
+
+    [Fact]
+    public void Equals_DifferentValues_ReturnsFalse()
+    {
+        var obj1 = new TestValueObject("abc", 123);
+        var obj2 = new TestValueObject("xyz", 123);
+
+        Assert.False(obj1.Equals(obj2));
+    }
+
+    [Fact]
+    public void GetHashCode_SameValues_ReturnsSameHashCode()
+    {
+        var obj1 = new TestValueObject("abc", 123);
+        var obj2 = new TestValueObject("abc", 123);
+
+        Assert.Equal(obj1.GetHashCode(), obj2.GetHashCode());
+    }
+
+    [Fact]
+    public void GetHashCode_DifferentValues_ReturnsDifferentHashCode()
+    {
+        var obj1 = new TestValueObject("abc", 123);
+        var obj2 = new TestValueObject("xyz", 123);
+
+        Assert.NotEqual(obj1.GetHashCode(), obj2.GetHashCode());
+    }
+}
---
+++ Slon.SharedAssets/Domain/ValueObject.cs
@@ -0,0 +1,40 @@
+﻿namespace Slon.SharedAssets.Domain;
+
+/// <summary>
+/// Represents an abstract base class for value objects.
+/// </summary>
+public abstract class ValueObject
+{
+    /// <summary>
+    /// Gets the equality components of the value object.
+    /// </summary>
+    /// <returns>An enumerable of the equality components.</returns>
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    /// <summary>
+    /// Determines whether the current value object is equal to another object.
+    /// </summary>
+    /// <param name="obj">The object to compare with the current value object.</param>
+    /// <returns><c>true</c> if the objects are considered equal; otherwise, <c>false</c>.</returns>
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    /// <summary>
+    /// Returns the hash code for the value object.
+    /// </summary>
+    /// <returns>A hash code value.</returns>
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x != null ? x.GetHashCode() : 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+}
