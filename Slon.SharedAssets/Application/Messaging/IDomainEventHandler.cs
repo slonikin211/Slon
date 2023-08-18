22da7d780258641410ea7d6a115bd7ef578cc55e
slonikin211 <85413294+slonikin211@users.noreply.github.com>
2023-08-18 19:24:01 +0400

    Application layer (#13)
    * Add interfaces for CQRS pattern
    
    * Add domain event handler
    
    * Add integration event interface
    
    * Add IAuditable interface

---
+++ Slon.SharedAssets/Application/Messaging/IDomainEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Slon.SharedAssets.Domain.Interfaces;
+
+namespace Slon.SharedAssets.Application.Messaging;
+
+#pragma warning disable CA1711 // Identifiers should not have incorrect suffix
+
+/// <summary>
+/// Represents a handler for domain events.
+/// </summary>
+public interface IDomainEventHandler : INotificationHandler<IDomainEvent>
+{
+}
+#pragma warning restore CA1711 // Identifiers should not have incorrect suffix
