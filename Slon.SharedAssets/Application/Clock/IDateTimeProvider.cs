7490b5e79100c71bbb064b9a6ae704e2f5b92522
slonikin211 <georgepark.dev@gmail.com>
2023-08-21 14:10:48 +0400

    Add xml comments

--- Slon.SharedAssets/Application/Clock/IDateTimeProvider.cs
+++ Slon.SharedAssets/Application/Clock/IDateTimeProvider.cs
@@ -1,6 +1,14 @@
 ﻿namespace Slon.SharedAssets.Application.Clock;
 
+/// <summary>
+/// Represents an interface for a date and time provider in an application.
+/// Date and time providers are used to obtain the current Coordinated Universal Time (UTC) time.
+/// </summary>
 public interface IDateTimeProvider
 {
+    /// <summary>
+    /// Gets the current Coordinated Universal Time (UTC) time.
+    /// </summary>
     DateTime UtcNow { get; }
 }
+
